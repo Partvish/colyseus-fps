c26f1321fd0ad397e2890a7d0c51b9b1c1dbfba3
Partvish <tagscherer.norbert@gmail.com>
2021-12-10 06:30:13 +0100

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,7 @@ app.use(express.json())
 
 const server = http.createServer(app);
 const gameServer = new Server({server,});
+gameServer.define('ArenaRoom', ArenaRoom);
 app.use("/colyseus", monitor());
 
 gameServer.listen(port);
